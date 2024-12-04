8ce0b516956a8f2da4609af827529497a6c48b66
chandanR <rc160098@gmail.com>
2024-12-04 13:22:12 +0530

    add

---
+++ index.js
@@ -0,0 +1,35 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'chandangowda2422@gmail.com',
+    pass: 'wwji mwpy wqlj brjs'
+  }
+});
+
+var mailOptions = {
+  from: 'chandangowda2422@gmail.com',
+  to: ['rc160098@gmail.com'],
+  subject: 'Sending Email using Node.js',
+  text: 'from chandan R',
+  html: '<h1>hello everyone</h1>',
+  attachments: [
+    {
+      filename: 'h1.jpeg',
+      path: './h1.jpeg', // Path to the file
+    },
+    {
+        filename: 'example.txt',
+        path: './example.txt', // Path to the file
+      },
+  ],
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
